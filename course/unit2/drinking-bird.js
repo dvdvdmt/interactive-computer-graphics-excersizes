71553ce4bd98dfb6a6d7ed52461e37cfa871b38c
Dmitriy Davydov <dvdvdmt@gmail.com>
2022-07-09 12:31:46 +0400

    Make the bird shiny and transparent

--- course/unit2/drinking-bird.js
+++ course/unit2/drinking-bird.js
@@ -1,52 +1,50 @@
 ////////////////////////////////////////////////////////////////////////////////
 /*global THREE, Coordinates, document, window  */
-var camera,
-  scene,
-  renderer;
-var cameraControls;
+var camera, scene, renderer
+var cameraControls
 
-var clock = new THREE.Clock();
+var clock = new THREE.Clock()
 
 function fillScene() {
-  scene = new THREE.Scene();
-  scene.fog = new THREE.Fog(0x808080, 2000, 4000);
+  scene = new THREE.Scene()
+  scene.fog = new THREE.Fog(0x808080, 2000, 4000)
 
   // LIGHTS
 
-  scene.add(new THREE.AmbientLight(0x222222));
+  scene.add(new THREE.AmbientLight(0x222222))
 
-  var light = new THREE.DirectionalLight(0xffffff, 0.7);
-  light.position.set(200, 500, 500);
+  var light = new THREE.DirectionalLight(0xffffff, 0.7)
+  light.position.set(200, 500, 500)
 
-  scene.add(light);
+  scene.add(light)
 
-  light = new THREE.DirectionalLight(0xffffff, 0.9);
-  light.position.set(-200, -100, -400);
+  light = new THREE.DirectionalLight(0xffffff, 0.9)
+  light.position.set(-200, -100, -400)
 
-  scene.add(light);
+  scene.add(light)
 
-  Coordinates.drawGround({size: 1000});
+  Coordinates.drawGround({size: 1000})
 
-  drawDrinkingBird();
+  drawDrinkingBird()
 }
 
 function drawDrinkingBird() {
   //////////////////////////////
   // MATERIALS
-  var headMaterial = new THREE.MeshLambertMaterial();
-  headMaterial.color.r = 104 / 255;
-  headMaterial.color.g = 1 / 255;
-  headMaterial.color.b = 5 / 255;
+  var headMaterial = new THREE.MeshLambertMaterial()
+  headMaterial.color.r = 104 / 255
+  headMaterial.color.g = 1 / 255
+  headMaterial.color.b = 5 / 255
 
-  var hatMaterial = new THREE.MeshPhongMaterial({shininess: 100});
-  hatMaterial.color.r = 24 / 255;
-  hatMaterial.color.g = 38 / 255;
-  hatMaterial.color.b = 77 / 255;
-  hatMaterial.specular.setRGB(0.5, 0.5, 0.5);
+  var hatMaterial = new THREE.MeshPhongMaterial({shininess: 100})
+  hatMaterial.color.r = 24 / 255
+  hatMaterial.color.g = 38 / 255
+  hatMaterial.color.b = 77 / 255
+  hatMaterial.specular.setRGB(0.5, 0.5, 0.5)
 
-  var bodyMaterial = new THREE.MeshPhongMaterial({shininess: 100});
-  bodyMaterial.color.setRGB(31 / 255, 86 / 255, 169 / 255);
-  bodyMaterial.specular.setRGB(0.5, 0.5, 0.5);
+  var bodyMaterial = new THREE.MeshPhongMaterial({shininess: 100})
+  bodyMaterial.color.setRGB(31 / 255, 86 / 255, 169 / 255)
+  bodyMaterial.specular.setRGB(0.5, 0.5, 0.5)
 
   // MODIFY THIS TO BE TRANSPARENT:
   var glassMaterial = new THREE.MeshPhongMaterial({
@@ -55,134 +53,132 @@ function drawDrinkingBird() {
     opacity: 0.3,
     transparent: true,
     specular: 0xffffff,
-  });
+  })
 
-  var legMaterial = new THREE.MeshPhongMaterial({shininess: 4});
-  legMaterial.color.setHex(0xada79b);
-  legMaterial.specular.setRGB(0.5, 0.5, 0.5);
+  var legMaterial = new THREE.MeshPhongMaterial({shininess: 4})
+  legMaterial.color.setHex(0xada79b)
+  legMaterial.specular.setRGB(0.5, 0.5, 0.5)
 
   var footMaterial = new THREE.MeshPhongMaterial({
     color: 0x960f0b,
     shininess: 30,
-  });
-  footMaterial.specular.setRGB(0.5, 0.5, 0.5);
+  })
+  footMaterial.specular.setRGB(0.5, 0.5, 0.5)
 
-  var sphere,
-    cylinder,
-    cube;
+  var sphere, cylinder, cube
 
-  var bevelRadius = 1.9; // TODO: 2.0 causes some geometry bug.
+  var bevelRadius = 1.9 // TODO: 2.0 causes some geometry bug.
 
   // MODELS
   // base
   cube = new THREE.Mesh(
     new THREE.BeveledBlockGeometry(20 + 64 + 110, 4, 2 * 77 + 12, bevelRadius),
-    footMaterial,
-  );
-  cube.position.x = -45; // (20+32) - half of width (20+64+110)/2
-  cube.position.y = 4 / 2; // half of height
-  cube.position.z = 0; // centered at origin
-  scene.add(cube);
+    footMaterial
+  )
+  cube.position.x = -45 // (20+32) - half of width (20+64+110)/2
+  cube.position.y = 4 / 2 // half of height
+  cube.position.z = 0 // centered at origin
+  scene.add(cube)
 
   // feet
   cube = new THREE.Mesh(
     new THREE.BeveledBlockGeometry(20 + 64 + 110, 52, 6, bevelRadius),
-    footMaterial,
-  );
-  cube.position.x = -45; // (20+32) - half of width (20+64+110)/2
-  cube.position.y = 52 / 2; // half of height
-  cube.position.z = 77 + 6 / 2; // offset 77 + half of depth 6/2
-  scene.add(cube);
+    footMaterial
+  )
+  cube.position.x = -45 // (20+32) - half of width (20+64+110)/2
+  cube.position.y = 52 / 2 // half of height
+  cube.position.z = 77 + 6 / 2 // offset 77 + half of depth 6/2
+  scene.add(cube)
 
   cube = new THREE.Mesh(
     new THREE.BeveledBlockGeometry(20 + 64 + 110, 52, 6, bevelRadius),
-    footMaterial,
-  );
-  cube.position.x = -45; // (20+32) - half of width (20+64+110)/2
-  cube.position.y = 52 / 2; // half of height
-  cube.position.z = -(77 + 6 / 2); // negative offset 77 + half of depth 6/2
-  scene.add(cube);
+    footMaterial
+  )
+  cube.position.x = -45 // (20+32) - half of width (20+64+110)/2
+  cube.position.y = 52 / 2 // half of height
+  cube.position.z = -(77 + 6 / 2) // negative offset 77 + half of depth 6/2
+  scene.add(cube)
 
-  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(64, 104, 6, bevelRadius), footMaterial);
-  cube.position.x = 0; // centered on origin along X
-  cube.position.y = 104 / 2;
-  cube.position.z = 77 + 6 / 2; // negative offset 77 + half of depth 6/2
-  scene.add(cube);
+  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(64, 104, 6, bevelRadius), footMaterial)
+  cube.position.x = 0 // centered on origin along X
+  cube.position.y = 104 / 2
+  cube.position.z = 77 + 6 / 2 // negative offset 77 + half of depth 6/2
+  scene.add(cube)
 
-  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(64, 104, 6, bevelRadius), footMaterial);
-  cube.position.x = 0; // centered on origin along X
-  cube.position.y = 104 / 2;
-  cube.position.z = -(77 + 6 / 2); // negative offset 77 + half of depth 6/2
-  scene.add(cube);
+  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(64, 104, 6, bevelRadius), footMaterial)
+  cube.position.x = 0 // centered on origin along X
+  cube.position.y = 104 / 2
+  cube.position.z = -(77 + 6 / 2) // negative offset 77 + half of depth 6/2
+  scene.add(cube)
 
   // legs
-  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(60, 282 + 4, 4, bevelRadius), legMaterial);
-  cube.position.x = 0; // centered on origin along X
-  cube.position.y = 104 + 282 / 2 - 2;
-  cube.position.z = 77 + 6 / 2; // negative offset 77 + half of depth 6/2
-  scene.add(cube);
+  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(60, 282 + 4, 4, bevelRadius), legMaterial)
+  cube.position.x = 0 // centered on origin along X
+  cube.position.y = 104 + 282 / 2 - 2
+  cube.position.z = 77 + 6 / 2 // negative offset 77 + half of depth 6/2
+  scene.add(cube)
 
-  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(60, 282 + 4, 4, bevelRadius), legMaterial);
-  cube.position.x = 0; // centered on origin along X
-  cube.position.y = 104 + 282 / 2 - 2;
-  cube.position.z = -(77 + 6 / 2); // negative offset 77 + half of depth 6/2
-  scene.add(cube);
+  cube = new THREE.Mesh(new THREE.BeveledBlockGeometry(60, 282 + 4, 4, bevelRadius), legMaterial)
+  cube.position.x = 0 // centered on origin along X
+  cube.position.y = 104 + 282 / 2 - 2
+  cube.position.z = -(77 + 6 / 2) // negative offset 77 + half of depth 6/2
+  scene.add(cube)
 
   // body
   sphere = new THREE.Mesh(
     new THREE.SphereGeometry(104 / 2, 32, 16, 0, Math.PI * 2, Math.PI / 2, Math.PI),
-    bodyMaterial,
-  );
-  sphere.position.x = 0;
-  sphere.position.y = 160;
-  sphere.position.z = 0;
-  scene.add(sphere);
+    bodyMaterial
+  )
+  sphere.position.x = 0
+  sphere.position.y = 160
+  sphere.position.z = 0
+  scene.add(sphere)
 
   // cap for top of hemisphere
-  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(104 / 2, 104 / 2, 0, 32), bodyMaterial);
-  cylinder.position.x = 0;
-  cylinder.position.y = 160;
-  cylinder.position.z = 0;
-  scene.add(cylinder);
+  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(104 / 2, 104 / 2, 0, 32), bodyMaterial)
+  cylinder.position.x = 0
+  cylinder.position.y = 160
+  cylinder.position.z = 0
+  scene.add(cylinder)
 
-  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(12 / 2, 12 / 2, 390 - 100, 32), bodyMaterial);
-  cylinder.position.x = 0;
-  cylinder.position.y = 160 + 390 / 2 - 100;
-  cylinder.position.z = 0;
-  scene.add(cylinder);
+  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(12 / 2, 12 / 2, 390 - 100, 32), bodyMaterial)
+  cylinder.position.x = 0
+  cylinder.position.y = 160 + 390 / 2 - 100
+  cylinder.position.z = 0
+  scene.add(cylinder)
 
   // glass stem
-  sphere = new THREE.Mesh(new THREE.SphereGeometry(116 / 2, 32, 16), glassMaterial);
-  sphere.position.x = 0;
-  sphere.position.y = 160;
-  sphere.position.z = 0;
-  scene.add(sphere);
+  sphere = new THREE.Mesh(new THREE.SphereGeometry(116 / 2, 32, 16), glassMaterial)
+  sphere.position.x = 0
+  sphere.position.y = 160
+  sphere.position.z = 0
+  scene.add(sphere)
 
-  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(24 / 2, 24 / 2, 390, 32), glassMaterial);
-  cylinder.position.x = 0;
-  cylinder.position.y = 160 + 390 / 2;
-  cylinder.position.z = 0;
-  scene.add(cylinder);
+  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(24 / 2, 24 / 2, 390, 32), glassMaterial)
+  cylinder.position.x = 0
+  cylinder.position.y = 160 + 390 / 2
+  cylinder.position.z = 0
+  scene.add(cylinder)
 
   // head
-  sphere = new THREE.Mesh(new THREE.SphereGeometry(104 / 2, 32, 16), headMaterial);
-  sphere.position.x = 0;
-  sphere.position.y = 160 + 390;
-  sphere.position.z = 0;
-  scene.add(sphere);
+  sphere = new THREE.Mesh(new THREE.SphereGeometry(104 / 2, 32, 16), headMaterial)
+  sphere.position.x = 0
+  sphere.position.y = 160 + 390
+  sphere.position.z = 0
+  scene.add(sphere)
 
   // hat
-  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(142 / 2, 142 / 2, 10, 32), hatMaterial);
-  cylinder.position.x = 0;
-  cylinder.position.y = 160 + 390 + 40 + 10 / 2;
-  cylinder.position.z = 0;
-  scene.add(cylinder);
+  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(142 / 2, 142 / 2, 10, 32), hatMaterial)
+  cylinder.position.x = 0
+  cylinder.position.y = 160 + 390 + 40 + 10 / 2
+  cylinder.position.z = 0
+  scene.add(cylinder)
 
-  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(80 / 2, 80 / 2, 70, 32), hatMaterial);
-  cylinder.position.x = 0;
-  cylinder.position.y = 160 + 390 + 40 + 10 + 70 / 2;
-  cylinder.position.z = 0;
-  scene.add(cylinder);
+  cylinder = new THREE.Mesh(new THREE.CylinderGeometry(80 / 2, 80 / 2, 70, 32), hatMaterial)
+  cylinder.position.x = 0
+  cylinder.position.y = 160 + 390 + 40 + 10 + 70 / 2
+  cylinder.position.z = 0
+  scene.add(cylinder)
 }
 
 function init() {
