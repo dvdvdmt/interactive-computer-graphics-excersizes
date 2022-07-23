44f005853c94e5b24926eb15b0378d378933ffb5
Dmitriy Davydov <dvdvdmt@gmail.com>
2022-07-23 10:56:56 +0400

    Add body to the arm

--- course/unit4/unit4-robot_arm_extended_exercise.js
+++ course/unit4/unit4-robot_arm_extended_exercise.js
@@ -66,19 +66,21 @@ function fillScene() {
 
   // Move the forearm itself to the end of the upper arm.
   forearm.position.y = uaLength
-  arm.add(forearm)
-  scene.add(arm)
 
   // YOUR CODE HERE
   body = new THREE.Object3D()
   var bodyLength = 60
+  arm.position.y = bodyLength
   // Add robot body here, put arm at top.
   // Note that "body" is already declared at top of this code.
   // Here's the call to create the body itself:
-  // createRobotBody( body, bodyLength, robotBodyMaterial );
+  createRobotBody(body, bodyLength, robotBodyMaterial)
   // ALSO CHECK OUT GUI CONTROLS FOR BODY
   // IN THE FUNCTIONS setupGUI() and render()
   // Note you'll have to add the body to the scene to get it to display.
+  arm.add(forearm)
+  body.add(arm)
+  scene.add(body)
 }
 
 function createRobotExtender(part, length, material) {
@@ -234,6 +236,8 @@ function render() {
   forearm.rotation.y = (effectController.fy * Math.PI) / 180 // yaw
   forearm.rotation.z = (effectController.fz * Math.PI) / 180 // roll
 
+  body.rotation.y = (effectController.bodyRotationY * Math.PI) / 180 // yaw
+
   renderer.render(scene, camera)
 }
 
@@ -246,7 +250,7 @@ function setupGui() {
     newAxes: axes,
 
     // UNCOMMENT FOLLOWING LINE TO SET DEFAULT VALUE OF CONTROLS FOR BODY:
-    // by: 0.0,
+    bodyRotationY: 0.0,
 
     uy: 70.0,
     uz: -15.0,
@@ -263,7 +267,7 @@ function setupGui() {
   h.add(effectController, 'newGround').name('Show ground')
   h.add(effectController, 'newAxes').name('Show axes')
   h = gui.addFolder('Arm angles')
-  // student, uncomment: h.add(effectController, "by", -180.0, 180.0, 0.025).name("Body y");
+  h.add(effectController, 'bodyRotationY', -180.0, 180.0, 0.025).name('Body rotation y')
   h.add(effectController, 'uy', -180.0, 180.0, 0.025).name('Upper arm y')
   h.add(effectController, 'uz', -45.0, 45.0, 0.025).name('Upper arm z')
   h.add(effectController, 'fy', -180.0, 180.0, 0.025).name('Forearm y')
